fa8be00e9e6b2c878d05d6cc9b300b555beff667
Joshua Crunden <joshuacrunden@gmail.com>
2022-06-03 12:44:21 +0100

    Add unit tests to test correct error types are returned

--- src/main_test.go
+++ src/main_test.go
@@ -85,12 +85,69 @@ func TestGetConcertsFromDynamoDBSucessful(t *testing.T) {
 	mockSvc := &mockDynamoDBClientSuccess{}
 	err := GetConcertsFromDynamoDB(mockSvc, &concerts)
 	if err != nil {
-		t.Errorf("Expected no error, go %s", err)
+		t.Errorf("Expected no error, got %s", err)
 	}
 
 	if len(concerts) != expectedNumConcerts {
 		t.Errorf("Expected %d concerts returned, got %d", expectedNumConcerts, len(concerts))
 	}
 
+	firstConcertDescription := "Summer Concert"
+	secondConcertDescription := "Winter Concert"
+
+	if concerts[0].Description != firstConcertDescription {
+		t.Errorf("Expected first concert returned to be %s, got %s", firstConcertDescription, concerts[0].Description)
+	}
+
+	if concerts[1].Description != secondConcertDescription {
+		t.Errorf("Expected second concert returned to be %s, got %s", secondConcertDescription, concerts[1].Description)
+	}
+}
+
+type mockDynamoDBClientNoConcerts struct {
+	dynamodbiface.DynamoDBAPI
+}
+
+func (m *mockDynamoDBClientNoConcerts) Scan(input *dynamodb.ScanInput) (response *dynamodb.ScanOutput, err error) {
+	numConcerts := 0
+	items := make([]map[string]*dynamodb.AttributeValue, 0)
+	numConcertsI64 := int64(numConcerts)
+	response = &dynamodb.ScanOutput{
+		Count: &numConcertsI64,
+		Items: items,
+	}
+	return
+}
+
+func TestGetConcertsFromDynamoDBNoConcerts(t *testing.T) {
+	expectedNumConcerts := 0
+	concerts := make([]Concert, 0, expectedNumConcerts)
+	mockSvc := &mockDynamoDBClientNoConcerts{}
+	err := GetConcertsFromDynamoDB(mockSvc, &concerts)
+	if err != err.(ErrNoConcerts) {
+		t.Errorf("Expected ErrNoConcerts type , got %s", err)
+	}
+
+	if len(concerts) != expectedNumConcerts {
+		t.Errorf("Expected %d concerts returned, got %d", expectedNumConcerts, len(concerts))
+	}
+}
+
+type mockDynamoDBClientResourceNotFound struct {
+	dynamodbiface.DynamoDBAPI
+}
+
+func (m *mockDynamoDBClientResourceNotFound ) Scan(input *dynamodb.ScanInput) (response *dynamodb.ScanOutput, err error) {
+	err = &dynamodb.ResourceNotFoundException{}
 	
+	return
+}
+
+func TestGetConcertsFromDynamoDBCannotAccessTable(t *testing.T) {
+	concerts := make([]Concert, 0)
+	mockSvc := &mockDynamoDBClientResourceNotFound {}
+	err := GetConcertsFromDynamoDB(mockSvc, &concerts)
+	if err != err.(*dynamodb.ResourceNotFoundException) {
+		t.Errorf("Expected %s error type, got %s", "ResourceNotFoundException", err)
+	}
 }
